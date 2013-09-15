e94a5cd2555dedb08194c92eb29a07a066ef133f
Lovie <lovie.minhas@gmail.com>
2013-09-15 09:07:16 +0100

    Added Help

--- README.txt
+++ README.txt
@@ -1,5 +1,8 @@
 Checks for Internet IP address changes and sends a mail with the new IP address incase it has.
 
+
+Step 1
+======
 Configure mail by installing
 
 sudo apt-get install ssmtp 
@@ -7,7 +10,9 @@ sudo apt-get install mailutils
 sudo apt-get install mpack
 
 
-Then edit mail config using:
+Step 2
+======
+Edit mail config using:
 
 sudo vi /etc/ssmtp/ssmtp.conf
 
@@ -19,3 +24,10 @@ FromLineOverride=YES
 mailhub=smtp.gmail.com:587
 UseSTARTTLS=YES
 
+Step 3
+======
+
+Install a crontab to run the script automatically
+
+Sample (Runs Daily at 5 AM):
+0 5 * * * sudo /home/pi/scripts/ipcheck/ipcheck.sh>/dev/null
--- ipcheck.sh
+++ ipcheck.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
+# Bash script to auto-verify Internet IP address changes using 'curl' and sends an email to a specified 
+# address in case the address is found to be changed.
+# Please add to crontab to run automatically.
 
-touch /tmp/ip.txt
+# Please reset these variables 
+export TMP_FILE_PATH=/tmp/ip.txt
+export USER_NAME="Lovie"
+export EMAIL_ADDRESS="lovie.minhas@gmail.com"
+
+touch $TMP_FILE_PATH
 export PUBLICIP=`curl ifconfig.me`
 if [ $? -eq 0 ]
 then
-	export OLDIP=`cat /tmp/ip.txt`
+	export OLDIP=`cat $TMP_FILE_PATH`
 	if [ "$OLDIP" != "$PUBLICIP" ]
 	then
-		echo "Hi Lovie, Your public IP has has changed to $PUBLICIP, Regards Pi"|mail -s "New IP $PUBLICIP" lovie.minhas@gmail.com
+		echo "Hi $USER_NAME, Your public IP has has changed to $PUBLICIP, Regards Pi"|mail -s "New IP $PUBLICIP" $EMAIL_ADDRESS
 		echo $PUBLICIP>/tmp/ip.txt
 	else
 		echo "No change to IP"
